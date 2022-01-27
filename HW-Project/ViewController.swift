120ca2dd634a97483316e0110ced4e4d87010c03
Chernyshov Igor <chernyshov.ig@yandex.ru>
2022-01-27 15:23:41 +0300

    Initial Commit

---
+++ HW-Project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW-Project
+//
+//  Created by Игорь Чернышов on 27.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
